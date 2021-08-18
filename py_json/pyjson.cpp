aeeab5d15bd83683608d064621cda101cc98bf3a
ziyanwu <juliawu9726@gmail.com>
2021-08-18 23:37:39 +0200

    update pyjson.cpp test.py

--- py_json/pyjson.cpp
+++ py_json/pyjson.cpp
@@ -5,6 +5,7 @@ using json = nlohmann::json;
 
 #include <pybind11/pybind11.h>
 #include "pybind11_json.hpp"
+#include <pybind11/numpy.h>
 namespace py = pybind11;
 
 void take_json(const json& s) {
@@ -19,6 +20,56 @@ json return_json(py::dict obj) {
     return j;
 }
 
+void sum_array(py::array_t<double> array){
+    py::buffer_info buf = array.request();
+    double sum = 0;
+    double *ptr = static_cast<double *>(buf.ptr);
+    for (size_t idx = 0; idx < buf.shape[0] * buf.shape[1]; idx++){
+        sum += ptr[idx];
+    }
+    std::cout << sum <<'\n';
+};
+
+
+void np2json(py::array_t<double> array, std::string type){
+//    json j;
+//    j[type] = array;  // Error: json not accept this type
+    json j2 = {
+            {"pi", 3.141},
+            {"happy", true},
+            {"name", "Niels"},
+            {"nothing", nullptr},
+            {"answer", {
+                {"everything", 42}
+            }},
+            {"list", {1, 0, 2}},
+//            {"array",{array}},  // Error:
+            {"object", {
+                {"currency", "USD"},
+                {"value", 42.99}
+            }}
+            };
+    std::cout<<j2<<'\n';
+}
+
+//bool validate_with_numpy(numpy&np; std::string s1
+//                        double tol_snap=0.001,
+//                        double planarity_d2p_tol=0.01,
+//                        double planarity_n_tol=20.0,
+//                        double overlap_tol=-1.0){
+//    json j;
+//    j[s1] = np;
+//    return val3dity::validate_one_geom(j,tol_snap,planarity_d2p_tol,planarity_n_tol,overlap_tol);
+//}
+
+bool validate_one_geom(json& j,
+                       double tol_snap=0.001,
+                       double planarity_d2p_tol=0.01,
+                       double planarity_n_tol=20.0,
+                       double overlap_tol=-1.0){
+    return val3dity::validate_one_geom(j,tol_snap,planarity_d2p_tol,planarity_n_tol,overlap_tol);
+}
+
 bool validate_cityjson(json& j,
         double tol_snap=0.001,
         double planarity_d2p_tol=0.01,
@@ -31,9 +82,16 @@ PYBIND11_MODULE(val3ditypy, m) {
     m.doc() = "My awesome module";
 
     m.def("take_json", &take_json, "pass py::object to a C++ function that takes an nlohmann::json");
+    //py::dictionary to json
     m.def("return_json", &return_json, "return py::object from a C++ function that returns an nlohmann::json");
+    //transform numpy to json
+    m.def("np2json", &np2json, "return py::numpy from a C++ function that returns an nlohmann::json");
     //val3dity
     m.def("validate_cityjson", &validate_cityjson, "A function validate cityjson",
+          py::arg("j"),  py::arg("tol_snap")=0.001,py::arg("planarity_d2p_tol")=0.01,
+          py::arg("planarity_n_tol")=20.0,py::arg("overlap_tol")=-1.0
+                  );
+    m.def("validate_one_geom", &validate_one_geom, "A function validate cityjson",
           py::arg("j"),  py::arg("tol_snap")=0.001,py::arg("planarity_d2p_tol")=0.01,
           py::arg("planarity_n_tol")=20.0,py::arg("overlap_tol")=-1.0
                   );
--- py_json/test.py
+++ py_json/test.py
@@ -1,6 +1,11 @@
 import val3ditypy
 import json
+import numpy as np
 
+
+'''
+use c++ function in python
+'''
 # val3dity
 path = "/home/ziyanwu/data/cityjson/cube.json"
 with open(path, 'r') as f:
@@ -14,4 +19,22 @@ j = val3ditypy.return_json(cityjson)
 # print('j:', j)
 # print('type(j): ', type(j))
 
-print(val3ditypy.validate_cityjson(j,0.001,0.01,20.0,-1.0))
+print(val3ditypy.validate_cityjson(j, 0.001, 0.01, 20.0, -1.0))
+
+
+
+
+vertices = np.array([[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0], [0, 0, 1], [1, 0, 1], [1, 1, 1], [0, 1, 1]])
+# print('vertices:\n', vertices)
+v_dic = {"vertices": vertices}
+print('v_dic:\n', v_dic)
+val3ditypy.np2json(vertices,"vertices")
+
+
+ls_vertices = [[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0], [0, 0, 1], [1, 0, 1], [1, 1, 1], [0, 1, 1]]
+lsv_dic = {"vertices": ls_vertices}
+print('lsv_dic:\n', lsv_dic)
+# print('type(v_dic): ', type(v_dic))
+j_lsv = val3ditypy.return_json(lsv_dic)
+print('j_lsv: \n', j_lsv)
+print(val3ditypy.validate_one_geom(j_lsv, 0.001, 0.01, 20.0, -1.0))
