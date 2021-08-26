73145c2f1697d282584df997a3073fc3a7438e89
ziyanwu <juliawu9726@gmail.com>
2021-08-26 23:57:15 +0200

    add test of validate_arrays

--- py_json/test.py
+++ py_json/test.py
@@ -2,52 +2,58 @@ import val3ditypy
 import json
 import numpy as np
 
-'''
-open from local path
-'''
+#========================
+### open from local path
+#=========================
 print("======open from local path======")
 
 # validate_cityjson
+print("1.validate cityjson file\n")
+
 path1 = "/home/ziyanwu/data/cityjson/cube.json"
-print("use [" + path1 + "] as example:")
+print("(use [" + path1 + "] as example:)")
 with open(path1, 'r') as f:
     cityjson = json.load(f)
-print("1.validate cityjson file")
+
 print(val3ditypy.validate_cityjson(cityjson, 0.001, 0.01, 20.0, -1.0))
 # with report
+print("#=== validate_cityjson report===#")
 cityjson_report = {}
 print(val3ditypy.validate_cityjson_jr(cityjson, cityjson_report, 0.001, 0.01, 20.0, -1.0))
 
 
-
 # validate_tu3djson
+print("2.validate tu3djson file\n")
+
 path2 = "/home/ziyanwu/data/cube.json"
-print("use [" + path2 + "] as example:")
+print("(use [" + path2 + "] as example:)")
 with open(path2, 'r') as f:
     tu3djson = json.load(f)
-print("2.validate tu3djson file")
+
 print(val3ditypy.validate_tu3djson(tu3djson, 0.001, 0.01, 20.0, -1.0))
 # with report
+print("#===validate_tu3djson report===#")
 tu3djson_report = {}
 print(val3ditypy.validate_tu3djson_jr(tu3djson, tu3djson_report, 0.001, 0.01, 20.0, -1.0))
 
 
 
 # validate_one_geom
-print("3.validate one_geom")
+print("3.validate one_geom\n")
 geometry = tu3djson["features"][0]["geometry"]
 print(val3ditypy.validate_one_geom(geometry, 0.001, 0.01, 20.0, -1.0))
 # with report
+print("#===validate_one_geom report===#")
 one_geom_report = {}
 print(val3ditypy.validate_one_geom_jr(geometry, one_geom_report, 0.001, 0.01, 20.0, -1.0))
 
 
 
 print('\n')
-'''
-use list and dictionary create cube_dic then transform to json format
-call validate function from val3dity
-'''
+#=========================================================================================
+##  use list and dictionary create cube_dic then transform to json format
+##  call validate function from val3dity
+#=========================================================================================
 print("======created file by hand======")
 # create file
 ls_boundaries = [[[[0, 3, 2, 1]], [[4, 5, 6, 7]], [[0, 1, 5, 4]], [[1, 2, 6, 5]], [[2, 3, 7, 6]], [[3, 0, 4, 7]]]]
@@ -57,7 +63,7 @@ cube_dic = {"version": "1.0", "type": "CityJSON", "vertices": ls_vertices,
                                      "geometry": [{"boundaries": ls_boundaries, "type": "Solid", "lod": 1}]}}}
 
 # validate_cityjson
-print("4.validate cityjson file")
+print("4.validate cityjson file\n")
 print(val3ditypy.validate_cityjson(cube_dic, 0.001, 0.01, 20.0, -1.0))
 
 # lsv_dic = {"vertices": ls_vertices}
@@ -70,3 +76,17 @@ print(val3ditypy.validate_cityjson(cube_dic, 0.001, 0.01, 20.0, -1.0))
 
 # print("jreport",jreport)
 # print("type:jreport",type(jreport))
+
+
+
+print('\n')
+#=========================================================================================
+##  use list create boundaries and vertices
+##  call validate_array function from val3dity
+#=========================================================================================
+print("======created boundaries and vertices by hand, call validate_array======")
+boundaries = [[[0,3,2,1]],[[4,5,6,7]],[[0,1,5,4]],[[1,2,6,5]],[[2,3,7,6]],[[3,0,4,7]]]
+vertices = [[0.0,0.0,0.0],[1.0,0.0,0.0],[1.0,1.0,0.0],[0.0,1.0,0.0],
+            [0.0,0.0,1.0],[1.0,0.0,1.0],[1.0,1.0,1.0],[0.0,1.0,1.0]]
+print(val3ditypy.validate_arrays("Solid",boundaries,vertices))
+
